73e4583365cee3e1d4440da854fb91d0965cdaa9
Jen Lipton <jkl.lipton@gmail.com>
2018-04-18 16:59:40 -0700

    unit tests

--- test/unit/Circus.test.js
+++ test/unit/Circus.test.js
@@ -5,29 +5,39 @@ describe('circus model', () => {
 
     it('valid good model', () => {
         const data = {
-            name: 'Monkey D. Luffy',
-            role: 'captain',
-            crew: 'Straw Hats',
-            joined: new Date(),
-            wardrobe: {
-                hat: 'straw',
-                top: 'red vest',
-                bottom: 'trousers',
-                shoes: 'flip flops'
-            },
-            bounty: 300000000,
-            weapons: [{
-                type: 'punch',
-                damage: 14
-            }]
+            title: 'Monty Python\'s Flying',
+            activities: ['british humor', 'math'],
+            launched: new Date(),
         };
 
         const circus = new Circus(data);
 
+        data.stops = [];
         data._id = circus._id;
-        data.weapons[0]._id = circus.weapons[0]._id;
         assert.deepEqual(circus.toJSON(), data);
 
         assert.isUndefined(circus.validateSync());
+    });
+
+    it('has default date of now', () => {
+        const circus = new Circus({ name: 'bob' });
+        assert.ok(circus.launched);
+        assert.isAtMost(circus.launched - Date.now(), 5);
+    });
+
+    it('required fields', () => {
+        const circus = new Circus({});
+        const { errors } = circus.validateSync();
+        assert.equal(Object.keys(errors).length, 1);
+        assert.equal(errors.title.kind, 'required');
+    });
+
+    it('activity length', () => {
+        const circus = new Circus({
+            activities: ['AxAZGgar1V0x0fdMwwgQnKiGHRAL2zJbFWnBkhYoytQ0Xh1trX0otTci5Gmsv6WhkrEBHARzzqnPSKg0zjfk8AvdoDQTjNNzF45V44']
+        });
+        const { errors } = circus.validateSync();
+        assert.equal(Object.keys(errors).length, 2);
+        assert.equal(errors['activities.0'].kind, 'maxlength');
     });
 });
