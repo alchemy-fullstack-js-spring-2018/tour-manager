dc372efb8d2d9599db3de9e6accbeed16b70be0e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-19 11:00:49 -0700

    fixed post test

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -31,16 +31,17 @@ describe('Tour API', () => {
     //         });
     // });
 
-    // it('it saves a tour', () => {
-    //     return request.post('/tours')
-    //         .send(volta)
-    //         .then(({ body }) => {
-    //             const { _id, __v, launchDate } = body;
-    //             assert.ok(_id);
-    //             assert.equal(__v, 0);
-    //             assert.ok(launchDate);
-    //             assert.deepEqual(body, { _id, __v, launchDate, ...volta });
-    //             volta = body;
-    //         });
-    // });
+    it('it saves a tour', () => {
+        return request.post('/tours')
+            .send(volta)
+            .then(({ body }) => {
+                console.log('BODY!!!', body);
+                const { _id, __v, launchDate } = body;
+                assert.ok(_id);
+                assert.equal(__v, 0);
+                assert.ok(launchDate);
+                assert.deepEqual(body, { _id, __v, launchDate, ...volta });
+                volta = body;
+            });
+    });
 });
