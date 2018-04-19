f5196a27226037a8913a5a331d6e387f0810a565
victorbofill <victorm.bofill@gmail.com>
2018-04-19 12:02:13 -0700

    changed ports to work with travis

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connect = require('./lib/utils/connect');
 
 const PORT = process.env.PORT || 3000;
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27019/tours';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/tours';
 
 connect(MONGODB_URI);
 
--- test/e2e/db.js
+++ test/e2e/db.js
@@ -2,7 +2,7 @@ require('dotenv').config({path: './test/e2e/.env' });
 const connect = require('../../lib/utils/connect');
 const mongoose = require('mongoose');
 
-before(() => connect(process.env.MONGODB_URI || 'mongodb://mongodb@localhost:27019/tours'));
+before(() => connect(process.env.MONGODB_URI || 'mongodb://mongodb@localhost:27017/tours'));
 after(() => mongoose.connection.close());
 
 module.exports = {
