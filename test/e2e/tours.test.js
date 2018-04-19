a0c7a53b34ee6f2e4661312ef2c978630102c6f8
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-19 11:17:27 -0700

    bypassing routes, successful "save" and "get" from db

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -6,8 +6,36 @@ const { dropCollection } = require('./db');
 describe('Tour API', () => {
     before(() => dropCollection('tours'));
 
-    it('connects', () => {
-        assert.ok(true);
-    });
+    let moMy = {
+        title: 'Mongo Mystique',
+        activities: ['curly brace juggling', 'id clowning', 'dollar sign magic', 'schema swallowing'],
+        launchDate: new Date(),
+        stops: []
+    };
 
-});
+    let maMo = {
+        title: 'Marvelous Mongoose',
+        activities: ['data type taming', 'document contortion'],        
+        stops: []
+    };
+
+    it('saves and gets a tour', () => {
+        return new Tour(moMy).save()
+            .then(saved => {
+                saved = saved.toJSON();
+                const { _id, __v, launchDate } = saved;
+                assert.ok(_id);
+                assert.equal(__v, 0);
+                assert.ok(launchDate);
+                assert.deepEqual(saved, {
+                    _id, __v, launchDate,
+                    ...moMy
+                });
+                moMy = saved;
+                return Tour.findById(saved._id).lean();
+            })
+            .then(found => {
+                assert.deepEqual(found, moMy);
+            });
+    });
+}); 
