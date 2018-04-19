13d252dc85b5316109310564963bae1fd56d37e4
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-19 10:48:40 -0700

    set up database connections

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 const app = require('./lib/app');
+const connect = require('./lib/util/connect');
 
 const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/tours';
+
+connect(MONGODB_URI);
 
 const server = http.createServer(app);
 
