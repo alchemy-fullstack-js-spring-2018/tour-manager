68d5ee342b4f12fa8109f85f1f3f5e35ec9d19ba
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-19 09:41:47 -0700

    valid model

--- test/unit/Tour.test.js
+++ test/unit/Tour.test.js
@@ -40,5 +40,48 @@ describe ('tour model', () => {
         data.stops[1]._id = tour.stops[1]._id;
 
         assert.deepEqual(tour.toJSON(), data);
+        assert.isUndefined(tour.validateSync());
+    });
+
+    it('default date is now', () => {
+        const tour = new Tour({});
+        assert.ok(tour.launchDate);
+        assert.isAtMost(tour.launchDate - Date.now(), 5);
+
+    });
+
+    const getValidationErrors = validation => {
+        assert.isDefined(validation, 'expected validation errors but got none');
+        return validation.errors;
+    };
+
+    it('required fields', () => {
+        const tour = new Tour({});
+        const errors = getValidationErrors(tour.validateSync());
+        assert.equal(Object.keys(errors).length, 1);
+        assert.equal(errors.title.kind, 'required');
+    });
+
+    it('attendance min is 1', () => {
+        const badAttendance = {
+            title: 'Clown Fiesta',
+            activities: ['Clown Boxing', 'Pumpkin Smashing', 'Air Ukelele'],
+            launchDate: new Date(),
+            stops: [{
+                location: {
+                    city: 'Seattle',
+                    state: 'WA',
+                    zip: '98101'
+                },
+                weather: {
+                    temperature: '72',
+                    condition: 'good'
+                },
+                attendance: 0
+            }]
+        };
+        const tour = new Tour(badAttendance);
+        const errors = getValidationErrors(tour.validateSync());
+        assert.equal(errors['stops.0.attendance'].kind, 'min');
     });
 });
