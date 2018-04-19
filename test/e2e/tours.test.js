e9918e8c52fa3a7c09074fb02540df3f1b160639
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-19 09:37:11 -0700

    e2e setup

---
+++ test/e2e/tours.test.js
@@ -0,0 +1,8 @@
+const { assert } = require('chai');
+const request = require('./request');
+const Tour = require('../../lib/models/Tour');
+const { dropCollection } = require('./db');
+
+describe('Tour API', () => {
+    
+});
