dc5439f2fe38efe4b49f3ec9325342dd18c45ac8
Jack Lomax <lomax15@me.com>
2018-04-19 11:09:38 -0700

    created schema and test/havent ran test

---
+++ test/unit/tour.test.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+const Tour = require('../../lib/models/tourSchema');
+
+describe('Tour model', () => {
+
+    it('is a valid model', () => {
+        const data = {
+            title: 'The Illusionists',
+            activities: ['magic', 'card tricks', 'illusions'],
+            launchDate: new Date(),
+            stops: [{
+                location: {
+                    city: 'Portland',
+                    state: 'OR',
+                    zip: '97239'
+                },
+                attendance: {
+                    type: 8160,
+                },
+                weather: {
+                    temperature: '65',
+                    sunset: '6pm'
+                },
+            }]
+        };
+
+        const tour = new Tour(data);
+        data._id = tour._id;
+        data.stops[0]._id = tour.stops[0]._id;
+        assert.deepEqual(tour.toJSON(), data);  
+    });
+});
