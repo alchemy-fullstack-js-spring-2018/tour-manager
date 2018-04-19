295d3b66cddcde61f6cfdd927a2e25ae939c2767
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-19 11:03:23 -0700

    removed comments

--- test/e2e/tours.test.js
+++ test/e2e/tours.test.js
@@ -10,26 +10,8 @@ describe('Tour API', () => {
     let volta = {
         title: 'VOLTA',
         activities: ['street sports', 'gameshow', 'acrobatics'],
-        // launchDate: new Date(),
         stops: []
     };
-
-    // it('it saves a tour', () => {
-    //     return new Tour(volta).save()
-    //         .then(saved => {
-    //             saved = saved.toJSON();
-    //             const { _id, __v, launchDate } = saved;
-    //             assert.ok(_id);
-    //             assert.equal(__v, 0);
-    //             assert.ok(launchDate);
-    //             assert.deepEqual(saved, { _id, __v, launchDate, ...volta });
-    //             volta = saved;
-    //             return Tour.findById(saved._id).lean();
-    //         })
-    //         .then(found => {
-    //             assert.deepEqual(found, volta);
-    //         });
-    // });
 
     it('it saves a tour', () => {
         return request.post('/tours')
