49cc0e9ee00c635f656269e655052e35fab4fbf9
CharlyWelch <blwbiology@gmail.com>
2018-04-19 09:16:46 -0700

    test required fields, enum, and min of Tour model

--- test/unit/Tour.test.js
+++ test/unit/Tour.test.js
@@ -26,4 +26,56 @@ describe('Tour model', () => {
         assert.deepEqual(tour.toJSON(), { _id: tour._id, ...data });
         assert.isUndefined(tour.validateSync());
     });
+
+    const getValidationErrors = validation => {
+        assert.isDefined(validation, 'expected errors but got none');
+        return validation.errors;
+    };
+
+    it('requires required fields', () => {
+        const tour = new Tour({});
+        const errors = getValidationErrors(tour.validateSync());
+        assert.equal(errors.title.kind, 'required');
+    });
+
+    it('throws error if state is not in enum', () => {
+        const testTour = new Tour({
+            title: 'Boom',
+            activities: ['goat-taming', 'dancing bears', 'lion snuggling'],
+            launchDate: new Date(),
+            stops: [{
+                location: {
+                    city: 'Gary',
+                    state: 'AA'
+                },
+                weather: {
+                    temperature: 102,
+                },
+                attendance: 3
+            }]
+        });
+
+        const errors = getValidationErrors(testTour.validateSync());
+        assert.equal(errors['stops.0.location.state'].kind, 'enum');
+    });
+
+    it('throws error if attendance is < 1', () => {
+        const testTour2 = new Tour({
+            title: 'Boom',
+            activities: ['fire-breathing', 'acrobatics', 'lion snuggling'],
+            launchDate: new Date(),
+            stops: [{
+                location: {
+                    city: 'Gary',
+                    state: 'IN'
+                },
+                weather: {
+                    temperature: 102,
+                },
+                attendance: 0
+            }]
+        });
+        const errors = getValidationErrors(testTour2.validateSync());
+        assert.equal(errors['stops.0.attendance'].kind, 'min');
+    });
 });
