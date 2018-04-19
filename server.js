411f9a49a46901eac9714cde2133ec1f29e280be
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-19 11:06:06 -0700

    Server functions, filling out base structure

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const connect = require('./lib/connect');
+
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/circus';
+
+connect(MONGODB_URI);
 
 const server = http.createServer(app);
 
+server.listen(PORT, () => {
+    console.log('I\'m givin\' her all she\'s got, Captain!!!', server.address().PORT);
+});
