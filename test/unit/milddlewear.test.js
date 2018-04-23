f7be96f5848b0851d4fbe4a9e64cb2bcbda1e032
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-23 07:23:47 -0700

    adding weather and location to test

--- test/unit/milddlewear.test.js
+++ test/unit/milddlewear.test.js
@@ -2,14 +2,21 @@ const { assert } = require('chai');
 
 function createWeatherLocationMiddleware(api){
     return (req, res, next) => {
-        api(req.body.zip);
+        api(req.body.zip)
+            .then(({ weather, location }) => {
+                req.body.weather = weather;
+                req.body.location = location;
+            });
     };
 }
 
 it.only('puts details about the weather on req.body based on zip', () => {
 
+    const weather = {};
+    const location = {};
     const api = zip => {
         api.zip = zip;
+        return Promise.resolve({ weather, location });
     };
 
     const middleware = createWeatherLocationMiddleware(api);
@@ -24,4 +31,7 @@ it.only('puts details about the weather on req.body based on zip', () => {
     middleware(req, null, next);
 
     assert.equal(api.zip, zip);
+    assert.equal(req.body.weather, weather);
+    assert.equal(req.body.location, location);
+
 });
